b5d7a3e495c91a7d52b8e56583d0c247dd66b969
chervy <chervy@student.42lyon.fr>
2025-01-28 14:43:12 +0100

    Up ft_strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,13 +1,13 @@
-section .text                   ; Section declaration
-
-; Declaration for the linker (ld)
+section .text
 global ft_strlen
 
 ; size_t ft_strlen(const char *s);
 
+;               rax,    rdi,    rsi,    rdx,    rcx,    r8,     r9
+; ft_strlen,    -,      s,      -,      -,      -,      -,      -
+
 ft_strlen:
     xor rax, rax                ; rax = 0
-    jmp .loop                   ; Jump to .loop
 .loop:
     cmp byte [rdi + rax], 0     ; Check if byte is 0.
     je .end                     ; If byte is 0, jump to end
